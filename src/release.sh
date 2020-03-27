599f5a1d23c18a280b52dde61df96d7c02fa9c85
Chris <github.account@chrigel.net>
2020-03-27 15:30:08 +0100

    Add image-cleanup deprecation notice

--- src/release.sh
+++ src/release.sh
@@ -44,6 +44,8 @@ seiso_version=$(curl -sS https://${api_user}api.github.com/repos/appuio/seiso/re
       '.[]| select(.prerelease|not) | .tag_name' \
   | head -n 1)
 
+image_cleanup_version=v0.3.0-rc.1 # last supported version before the breaking changes
+
 kubeval_version=$(curl -sS https://${api_user}api.github.com/repos/instrumenta/kubeval/releases \
   | jq --raw-output \
       '.[]| select(.prerelease|not) | .tag_name' \
@@ -58,6 +60,10 @@ kustomize_shasum=$(curl -sSL https://github.com/kubernetes-sigs/kustomize/releas
   | cut -f 1 -d ' ')
 
 seiso_shasum=$(curl -sSL https://github.com/appuio/seiso/releases/download/${seiso_version}/checksums.txt \
+  | grep linux_amd64 \
+  | cut -f 1 -d ' ')
+
+image_cleanup_shasum=$(curl -sSL https://github.com/appuio/seiso/releases/download/${image_cleanup_version}/checksums.txt \
   | grep linux_amd64 \
   | cut -f 1 -d ' ')
 
@@ -76,6 +82,7 @@ echo "- helm2: ${helm2_version} (shasum: ${helm2_shasum})"
 echo "- helm3: ${helm3_version} (shasum: ${helm3_shasum})"
 echo "- kustomize: ${kustomize_version} (shasum: ${kustomize_shasum})"
 echo "- seiso: ${seiso_version} (shasum: ${seiso_shasum})"
+echo "- image-cleanup: ${image_cleanup_version} (shasum: ${image_cleanup_shasum})"
 echo "- kubeval: ${kubeval_version} (shasum: ${kubeval_shasum})"
 echo "- sops: ${sops_version}"
 
@@ -91,6 +98,7 @@ curl -sSL "$url" \
         -e "s/%%HELM3_VERSION%%/${helm3_version}/" \
         -e "s/%%KUSTOMIZE_VERSION%%/${kustomize_version}/" \
         -e "s/%%SEISO_VERSION%%/${seiso_version}/" \
+        -e "s/%%IMAGE_CLEANUP_VERSION%%/${image_cleanup_version}/" \
         -e "s/%%KUBEVAL_VERSION%%/${kubeval_version}/" \
         -e "s/%%SOPS_VERSION%%/${sops_version}/" \
         -e "s/%%ARCHIVE%%/${archive}/" \
@@ -99,7 +107,9 @@ curl -sSL "$url" \
         -e "s/%%HELM3_SHA256SUM%%/${helm3_shasum}/" \
         -e "s/%%KUSTOMIZE_SHA256SUM%%/${kustomize_shasum}/" \
         -e "s/%%SEISO_SHA256SUM%%/${seiso_shasum}/" \
+        -e "s/%%IMAGE_CLEANUP_SHA256SUM%%/${image_cleanup_shasum}/" \
         -e "s/%%KUBEVAL_SHA256SUM%%/${kubeval_shasum}/" \
         src/Dockerfile > "${ver}/Dockerfile" && \
-      cp -r src/opt "${ver}/"
+      cp -r src/opt "${ver}/" && \
+      cp "src/image-cleanup.sh" "${ver}/"
     done
--- v3.6/opt/helm2-deprecation.sh
+++ v3.6/opt/helm2-deprecation.sh
@@ -7,21 +7,25 @@ CYAN='\033[1;36m'
 WHITE='\033[1;37m'
 NO_COLOR='\033[0m'
 
-echo -e ${BLUE}
-echo -e "----------------------------------------------------------------------"
-echo -e "${PURPLE}DEPRECATION NOTICE:${YELLOW} We're replacing helm version 2 by version 3."
-echo -e "  Please, migrate your setup! Helm v3 is incompatible with v2."
-echo -e "  Migration guide: ${WHITE}https://helm.sh/docs/topics/v2_v3_migration/"
-echo -e "${YELLOW}"
-echo -e "  The binary /bin/helm will point to /bin/helm3 starting June 1, 2020."
-echo -e "  Please update calls to 'helm' by 'helm3' before this change."
-echo
-echo -e "  The binary /bin/helm2 will be available until December 1, 2020."
-echo -e "  You may update calls to 'helm' by 'helm2' during that period."
-echo -e ${CYAN}
-echo -e "  Thank you for using APPUiO!"
-echo -e "  Technical support: ${WHITE}support@vshn.ch${BLUE}"
-echo -e "----------------------------------------------------------------------"
-echo -e ${NO_COLOR}
+echo_to_stderr() {
+  echo -e "${1}" >&2
+}
+
+echo_to_stderr ${BLUE}
+echo_to_stderr "----------------------------------------------------------------------"
+echo_to_stderr "${PURPLE}DEPRECATION NOTICE:${YELLOW} We're replacing helm version 2 by version 3."
+echo_to_stderr "  Please, migrate your setup! Helm v3 is incompatible with v2."
+echo_to_stderr "  Migration guide: ${WHITE}https://helm.sh/docs/topics/v2_v3_migration/"
+echo_to_stderr "${YELLOW}"
+echo_to_stderr "  The binary /bin/helm will point to /bin/helm3 starting June 1, 2020."
+echo_to_stderr "  Please update calls to 'helm' by 'helm2' before this change, if you don't migrate to v3."
+echo >&2
+echo_to_stderr "  The binary /bin/helm2 will be available as long as Helm v2 is officially supported."
+echo_to_stderr "  You may update calls to 'helm' by 'helm2' during that period."
+echo_to_stderr ${CYAN}
+echo_to_stderr "  Thank you for using APPUiO!"
+echo_to_stderr "  Technical support: ${WHITE}support@vshn.ch${BLUE}"
+echo_to_stderr "----------------------------------------------------------------------"
+echo_to_stderr ${NO_COLOR}
 
 helm2 ${*}
